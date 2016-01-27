a740f79af31d5c53d051f467c7ee1518b1c4ef85
Régis <regis.pierrard@supelec.fr>
2016-01-27 15:51:43 +0100

    Makefile corrected

--- src/knn.hpp
+++ src/knn.hpp
@@ -13,5 +13,5 @@
 double distanceEuclidienne(std::vector<std::complex<double>> data_vector, std::vector<std::complex<double>> prototype);
 double distanceHamming(std::vector<std::complex<double>> data_vector, std::vector<std::complex<double>> prototype);
 double distance(std::vector<std::complex<double>> data_vector, std::vector<std::complex<double>> prototype, double n);
-std::vector< std::pair<double, std::string> > kClosest(std::vector<std::complex<double>> data_vector, std::map< std::string, std::vector< std::vector< std::complex<double> > > > database_map, int K, doubles n);
+std::vector< std::pair<double, std::string> > kClosest(std::vector<std::complex<double>> data_vector, std::map< std::string, std::vector< std::vector< std::complex<double> > > > database_map, int K, double n);
 std::string knn (std::vector<std::complex<double>> data_vector, std::map< std::string, std::vector< std::vector< std::complex<double> > > > database_map, int K, double n);
